d5138258c45653ee91b735b08a01ed89d4cb2077
Ajitesh Abhishek <ajiteshleo@gmail.com>
2020-01-15 16:30:57 -0600

    AJAX implemented

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
@@ -16,17 +16,18 @@ class Todo(db.Model):
     def __repr__(self):
         return f("<Todo {self.id}: {self.description}>")
 
-db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description=request.get_json()['description']
+    todo=Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description':todo.description
+    })
+    #Index is the name of route handler for the home page
 
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
-
-@app.route('/todo/create', methods=['POST'])
-def create_todo():
-    description=request.form.get('description', '')
-    todo=Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
-    #Index is the name of route handler for the home page
