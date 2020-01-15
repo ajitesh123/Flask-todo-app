1746bdfeb3666c97b0ce207e4307e471aca611c1
Ajitesh Abhishek <ajiteshleo@gmail.com>
2020-01-15 17:20:24 -0600

    Adopted session closing pratice

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app=Flask(__name__)
 #Creates an application names after your file. It's an instance of flask
@@ -18,15 +19,29 @@ class Todo(db.Model):
 
 
 @app.route('/todos/create', methods=['POST'])
+'''Sort of function to call function. Decoraters could be used to call function multiple times.'''
 def create_todo():
-    description=request.get_json()['description']
-    todo=Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description':todo.description
-    })
-    #Index is the name of route handler for the home page
+    error = False
+    body = {}
+    try:
+        description=request.get_json()['description']
+        todo=Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+        #we have to do thsi here as todo.descritpion becomes out of date if we close the session. So we
+        #can't do this assignment in the jsonify stage
+    except:
+        error=True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+#Index is the name of route handler for the home page
 
 @app.route('/')
 def index():
